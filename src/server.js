23cacfae77ead0da65addabafa87ba6070bf57f6
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-10-27 15:53:45 -0300

    Rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
 import http from 'node:http';
 
+// GET -> Buscar uma informação
+// POST -> Criar uma informação
+// PUT -> Atualizar uma informação
+// DELETE -> Deletar uma informação
+// PATCH -> Atualizar uma informação específica
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello Word')
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+
+        return res.end('Listagem de usuários');
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criação de um usuário');
+    }
+
+    return res.end('Rota de escape');
+
 });
 
 server.listen(3333)
